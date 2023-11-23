d8dcbb57955d7006d284c535198db3224e3755f1
borjeb <borjeb+github@pm.me>
2023-11-23 10:16:56 +0100

    Update Update-PowerShellModules.ps1

--- Update-PowerShellModules.ps1
+++ Update-PowerShellModules.ps1
@@ -5,6 +5,7 @@ Update PowerShellGet and PackageManagement
 #>
 
 $WorkingDir = $env:TEMP
+$dist = 'C:\Program Files'
 
 #PowerShellGet from PSGallery URL
 if (!(Get-Module -Name PowerShellGet)){
@@ -12,9 +13,9 @@ if (!(Get-Module -Name PowerShellGet)){
     Invoke-WebRequest -UseBasicParsing -Uri $PowerShellGetURL -OutFile "$WorkingDir\powershellget.2.2.5.zip"
     $Null = New-Item -Path "$WorkingDir\2.2.5" -ItemType Directory -Force
     Expand-Archive -Path "$WorkingDir\powershellget.2.2.5.zip" -DestinationPath "$WorkingDir\2.2.5"
-    $Null = New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\PowerShellGet" -ItemType Directory -ErrorAction SilentlyContinue
-    Move-Item -Path "$WorkingDir\2.2.5" -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\PowerShellGet\2.2.5"
-    Remove-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\PowerShellGet\1.0.0.1" -Recurse -force
+    $Null = New-Item -Path "$dist\WindowsPowerShell\Modules\PowerShellGet" -ItemType Directory -ErrorAction SilentlyContinue
+    Move-Item -Path "$WorkingDir\2.2.5" -Destination $dist\WindowsPowerShell\Modules\PowerShellGet\2.2.5"
+    Remove-Item -Path "$dist\WindowsPowerShell\Modules\PowerShellGet\1.0.0.1" -Recurse -force
     }
 
 #PackageManagement from PSGallery URL
@@ -23,8 +24,8 @@ if (!(Get-Module -Name PackageManagement)){
     Invoke-WebRequest -UseBasicParsing -Uri $PackageManagementURL -OutFile "$WorkingDir\packagemanagement.1.4.8.1.zip"
     $Null = New-Item -Path "$WorkingDir\1.4.8.1" -ItemType Directory -Force
     Expand-Archive -Path "$WorkingDir\packagemanagement.1.4.8.1.zip" -DestinationPath "$WorkingDir\1.4.8.1"
-    $Null = New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\PackageManagement" -ItemType Directory -ErrorAction SilentlyContinue
-    Move-Item -Path "$WorkingDir\1.4.8.1" -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\PackageManagement\1.4.8.1"
+    $Null = New-Item -Path "$dist\WindowsPowerShell\Modules\PackageManagement" -ItemType Directory -ErrorAction SilentlyContinue
+    Move-Item -Path "$WorkingDir\1.4.8.1" -Destination "$dist\WindowsPowerShell\Modules\PackageManagement\1.4.8.1"
     }
     
 #PSReadline from PSGallery URL
@@ -33,6 +34,6 @@ if (!(Get-Module -Name PSReadline)){
     Invoke-WebRequest -UseBasicParsing -Uri $PSReadlineURL -OutFile "$WorkingDir\psreadline.2.3.4.zip"
     $Null = New-Item -Path "$WorkingDir\2.3.4" -ItemType Directory -Force
     Expand-Archive -Path "$WorkingDir\psreadline.2.3.4.zip" -DestinationPath "$WorkingDir\2.3.4"
-    $Null = New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\PSReadline" -ItemType Directory -ErrorAction SilentlyContinue
-    Move-Item -Path "$WorkingDir\2.3.4" -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\PSReadlin\2.3.4"
+    $Null = New-Item -Path "$dist\WindowsPowerShell\Modules\PSReadline" -ItemType Directory -ErrorAction SilentlyContinue
+    Move-Item -Path "$WorkingDir\2.3.4" -Destination "$dist\WindowsPowerShell\Modules\PSReadlin\2.3.4"
     }
